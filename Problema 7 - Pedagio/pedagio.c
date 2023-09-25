eb7a7c1eb08e40f190625a32644b43c15ea6ae83
DanCLima <daniel.costa@sou.unifal-mg.edu.br>
2023-09-25 00:16:28 -0300

    Problema 7 - Finalizado.

--- Problema 7 - Pedagio/pedagio.c
+++ Problema 7 - Pedagio/pedagio.c
@@ -1,3 +1,13 @@
+/*---------------------------------------------------------------+
+|            UNIFAL - Universidade Federal de Alfenas            |
+|              BACHARELADO EM CIÊNCIA DA COMPUTAÇÃO              |
+|       Trabalho....:   Problema 7                               |
+|       Disciplina..:   Desafios de Programação                  |
+|       Professor...:   Luiz Eduardo da Silva                    |
+|       Aluno.......:   Daniel da Costa Lima                     |
+|       Data........:   25/09/2023                               |
++----------------------------------------------------------------*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,14 +18,15 @@ typedef struct {
     char placa[21];
     int dia;
     int hora;
-    int sentido;        // 0 - entrar na rodovia 1 - sair da rodovia
+    int sentido;        // 0 - entrar na rodovia (enter)        1 - sair da rodovia (exit)
     int km;
-    int contabilizado;      // 0 - não foi usado na soma geral 1 - foi usado na soma geral
+    int contabilizado;      // 0 - não foi usado na soma geral      1 - foi usado na soma geral
     float valor;      
 } Viagem;
 
 Viagem registros[MAX];
 
+/* Inicia a struct */
 void iniciaViagem (Viagem *v) {
     for (int i = 0; i < MAX; i++) {
         v[i].placa[0] = '\0';
@@ -28,6 +39,7 @@ void iniciaViagem (Viagem *v) {
     }
 }
 
+/* Insere um registro na struct */
 void cadastraViagem (Viagem *v, char *placa, int dia, int hora, int sentido, int km, int indice) {
     if (indice >= MAX) {
         printf("Limite de viagens atingido!\n");
@@ -52,21 +64,28 @@ int removeQuebraLinha (char *buffer) {
     return tam;
 }
 
+/* Inicia o vetor ordem */
 void iniciaOrdem (int *ordem, int qtdRegistro) {
     for (int i = 0; i < qtdRegistro; i++) {
         ordem[i] = i;
     }
 }
 
+/* 
+    Realiza a ordenação do vetor que contém os índices das structs de acordo com alguns critérios:
+        1 - Placa
+        2 - Dia
+        3 - Hora
+ */
 int ordenaPlaca(const void *a, const void *b) {
     int indice1 = *(int *)a;
     int indice2 = *(int *)b;
     int retorno;
 
     retorno = strcmp(registros[indice1].placa, registros[indice2].placa);
-    if (retorno == 0) {     // Quer dizer que as placas são iguais, então devemos considerar outros critérios
+    if (retorno == 0) {     // Se as placas são iguais, então devemos considerar outros critérios
         retorno = (registros[indice1].dia - registros[indice2].dia);
-        if (retorno == 0) {     // Para os dis iguais, ordenaremos as horas
+        if (retorno == 0) {     // Se os dias são iguais, consideraremos as horas
             retorno = (registros[indice1].hora - registros[indice2].hora);
         } 
     }
@@ -74,11 +93,12 @@ int ordenaPlaca(const void *a, const void *b) {
     return retorno;
 }
 
-
+/* Utiliza o quick-sort para ordenador o vetor de índices */
 void ordena (Viagem *v, int *ordem, int qtdRegistro) {
     qsort(ordem, qtdRegistro, sizeof(int), ordenaPlaca);
 }
 
+/* DEPURAÇÃO - exibe a struct */
 void exibeViagem (Viagem *v, int *ordem, int qtdRegistros) {
         printf("\n");
 
@@ -96,6 +116,7 @@ void exibeViagem (Viagem *v, int *ordem, int qtdRegistros) {
     }
 }
 
+/* Calcula o valor do pedágio de acordo com a lógica do problema */
 void calculaPedagio (Viagem *v, int *ordem, int *taxas, int qtdRegistro) {
     for (int i = 0; i < qtdRegistro-1; i++) {
         float taxaAtual = 0.0;
@@ -103,27 +124,27 @@ void calculaPedagio (Viagem *v, int *ordem, int *taxas, int qtdRegistro) {
         int indice2 = ordem[i+1];
 
         if (strcmp(v[indice1].placa, v[indice2].placa) == 0 && v[indice1].sentido != v[indice2].sentido) {
-            /* Realizando o cálculo do pedágio e armazenando sempre na struct com o sentido enter */
+            /* Calcula o pedágio e armazena sempre na struct com o sentido enter (0)*/
             if (v[indice1].sentido == 0) {
-                taxaAtual = (taxas[v[indice1].hora])/100.00;       // Pegando a taxa no sentido que entrou na pista
-                v[indice1].valor += taxaAtual * abs(v[indice1].km - v[indice2].km);     // Realizando o cálculo do pedágio
+                taxaAtual = (taxas[v[indice1].hora])/100.00;       // A taxa muda de acordo com o horário. O horário corresponde aos índices do vetor taxas
+                v[indice1].valor += taxaAtual * abs(v[indice1].km - v[indice2].km);     // Realiza o cálculo do pedágio
             }
         }
     }
 
+    /* O objetivo dessa repetição é passar por todas as viagens válidas e armazenar a soma dos valores no primeiro registro da placa */
     for (int i = 0; i < qtdRegistro-1; i++) {
-        int indice1 = ordem[i];
+        int indice1 = ordem[i];     // Ajusta o índice, porque seguimos o vetor que ordenou os registros
         float contaViagens = 1.0;
 
-        /* Verificando se a primeira estrutura está no sentido enter */
+        /* Verifica se a primeira estrutura está no sentido enter (0) */
         if (v[indice1].contabilizado == 0 && v[indice1].sentido == 0 && v[indice1].valor != 0) {
 
             for (int j = i+1; j < qtdRegistro; j++) {
                 int indice2 = ordem[j];
-
-                /* A segunda placa tem que ser igual a primeira, o sentido também tem que ser enter e o valor diferente de 0 */
+                
+                /* O segundo registro deve possuir um valor válido sentido enter - 0 */
                 if (strcmp(v[indice1].placa, v[indice2].placa) == 0 && v[indice2].contabilizado == 0 && v[indice2].sentido == 0) {
-                    // printf("Entrou nas placas [%d]-%s [%d]-%s\n", indice1, v[indice1].placa, indice2, v[indice2].placa);
 
                     if (v[indice2].valor != 0) {
                         v[indice1].valor += v[indice2].valor;  
@@ -141,6 +162,7 @@ void calculaPedagio (Viagem *v, int *ordem, int *taxas, int qtdRegistro) {
     }
 }
 
+/* Exibe a saída de acordo com o proposto pelo exercício */
 void saidaEsperada (Viagem *v, int *ordem, int qtdRegistro) {
     for (int i = 0; i < qtdRegistro; i++) {
         int indice = ordem[i];
@@ -158,7 +180,7 @@ int main(int argc, char const *argv[])
     int mes, dia, hora, segundo, km;
     int contaEntrada, contaRegistro;
     char buffer[100];
-    char sentido[6]; // Entrada ou saida
+    char sentido[6]; // Entrada - 0     saida - 1
     char placa[21];
     int *ordem;     // Vetor que irá guardar a ordenação das structs
 
@@ -188,11 +210,11 @@ int main(int argc, char const *argv[])
                 saidaEsperada(registros, ordem, contaRegistro);
                 free(ordem);
 
-                iniciaViagem(registros);       // Reiniciando a struct para a nova entrada
+                /* Reinicia a struct para a nova entrada */
+                iniciaViagem(registros);       
             }
-            contaRegistro = 0;      // Sempre que houver uma nova entrada o conta registro deve ser zerado
+            contaRegistro = 0;      // Sempre que houver uma nova entrada, o conta registro deve ser zerado
     
-            // printf("Entrada %d\n", contaEntrada);
             /* Lendo as taxas */
             scanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n", &taxas[0], &taxas[1], &taxas[2], &taxas[3], 
                 &taxas[4], &taxas[5], &taxas[6], &taxas[7], &taxas[8], &taxas[9], &taxas[10], &taxas[11], &taxas[12], &taxas[13], &taxas[14],
