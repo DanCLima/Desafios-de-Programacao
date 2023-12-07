bc803d8eaff9c74414cd86444dbba9c2018720c7
DanCLima <daniel.costa@sou.unifal-mg.edu.br>
2023-12-07 16:48:49 -0300

    Problema 20 - Finalizado!

--- Problema 20 - LCD/lcd.c
+++ Problema 20 - LCD/lcd.c
@@ -1,38 +1,81 @@
+/*---------------------------------------------------------------+
+|            UNIFAL - Universidade Federal de Alfenas            |
+|              BACHARELADO EM CIÊNCIA DA COMPUTAÇÃO              |
+|       Trabalho....:   Problema 20                              |
+|       Disciplina..:   Desafios de Programação                  |
+|       Professor...:   Luiz Eduardo da Silva                    |
+|       Aluno.......:   Daniel da Costa Lima                     |
+|       Data........:   07/12/2023                               |
++----------------------------------------------------------------*/
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 
-int tabela[10][7] = {       // a, b, c, d, e, f, g
-    {1, 1, 1, 0, 1, 1, 1},  //0
-    {0, 0, 1, 0, 0, 1, 0},  //1
-    {1, 0, 1, 1, 1, 0, 1},  // 2
-    {1, 0, 1, 1, 0, 1, 1},  // 3
-    {0, 1, 1, 1, 0, 1, 0},  // 4
-    {1, 1, 0, 1, 0, 1, 1},  // 5
-    {1, 1, 0, 1, 1, 1, 1},  // 6
-    {1, 0, 1, 0, 0, 1, 0},  // 7
-    {1, 1, 1, 1, 1, 1, 1},  // 8
-    {1, 1, 1, 1, 0, 1, 1},  // 9
+int tabela[10][7] = {       // Setores [A, B, C, D, E, F, G]
+    {1, 1, 1, 0, 1, 1, 1},  // Digito 0
+    {0, 0, 1, 0, 0, 1, 0},  // Digito 1
+    {1, 0, 1, 1, 1, 0, 1},  // Digito 2
+    {1, 0, 1, 1, 0, 1, 1},  // Digito 3
+    {0, 1, 1, 1, 0, 1, 0},  // Digito 4
+    {1, 1, 0, 1, 0, 1, 1},  // Digito 5
+    {1, 1, 0, 1, 1, 1, 1},  // Digito 6
+    {1, 0, 1, 0, 0, 1, 0},  // Digito 7
+    {1, 1, 1, 1, 1, 1, 1},  // Digito 8
+    {1, 1, 1, 1, 0, 1, 1},  // Digito 9
 };
 
 void imprime_setor (int setor, int digito, int tam) {
     switch (setor) {
     case 0:
-        printf(" ");
-        for (int i = 0; i < tam; i++) {
-            printf("-");
-        }
-        printf(" ");
-        
-        break;    
-    case 7:
-        printf(" ");
-        for (int i = 0; i < tam; i++) {
+    case 3:
+    case 6:
+        if (tabela[digito][setor] == 1) {
             printf(" ");
+            for (int i = 0; i < tam; i++) {
+                printf("-");
+            }
+            printf("  ");
+        } else {
+            printf(" ");
+            for (int i = 0; i < tam; i++) {
+                printf(" ");
+            }
+            printf("  ");            
         }
-        printf(" ");
+        break;    
+    case 1:
+    case 4:
+        if (tabela[digito][setor] == 1) {
+            if (tabela[digito][setor+1] == 1) {
+                printf("|");
+                for (int i = 0; i < tam; i++) {
+                    printf(" ");
+                }
+                printf("| ");
+            } else {
+                printf("|");
+                for (int i = 0; i < tam; i++) {
+                    printf(" ");
+                }
+                printf("  ");
+            }
+        } else {
+            if (tabela[digito][setor+1] == 1) {
+                printf(" ");
+                for (int i = 0; i < tam; i++) {
+                    printf(" ");
+                }
+                printf("| ");
+            } else {
+                printf(" ");
+                for (int i = 0; i < tam; i++) {
+                    printf(" ");
+                }
+                printf("  ");
+            }
+        }
         break;
-    
     default:
         break;
     }
@@ -43,33 +86,34 @@ int main(int argc, char const *argv[])
     int tam;    
     char num[20];
 
-    scanf("%d %s", &tam, num);
+    while (scanf("%d %s", &tam, num) != EOF) {
+        if (tam == 0 && num[0] == '0') {
+            break;
+        } else {
+            int tamanho_numero = strlen(num);
+            for (int i = 0; i < 7; i++) {       // Loop que percorre todos os setores
+                if (i == 1 || i == 4) {
+                    int cont = 0;
+                    while (cont < tam) {
+                        for (int j = 0; j < tamanho_numero; j++) {      // Loop que percorre todos os dígitos
+                            int digito = num[j] - '0';      // Transformando o digito caracter em um digito int       
 
-    int tamanho_numero = strlen(num);
-    printf("Tam %d num %s tam_num %d\n", tam, num, tamanho_numero);
+                            imprime_setor(i, digito, tam);
+                        }
+                        printf("\n");
+                        cont++;          
+                    }
+                } else if (i == 0 || i == 3 || i == 6) {
+                    for (int j = 0; j < tamanho_numero; j++) {      // Loop que percorre todos os dígitos
+                        int digito = num[j] - '0';      // Transformando o digito caracter em um digito int
 
-    for (int i = 0; i < 7; i++) {       // Loop que percorre todos os setores
-        for (int j = 0; j < tamanho_numero; j++) {      // Loop que percorre todos os dígitos
-            int digito = num[j] - '0';      // Transformando o digito caracter em um digito int
-            
-            if ((i == 0 || i == 3 || i == 6) && tabela[digito][i] == 0) {
-                imprime_setor(7, digito, tam);
-                // if (i == 6) {
-                //     printf(" \n");
-                // } else {
-                //     printf(" ");
-                // }
-                
+                        imprime_setor(i, digito, tam);
+                    }
+                    printf("\n");
+                }
             }
-
-            if (tabela[digito][i] == 1) {
-                imprime_setor(i, digito, tam);
-            }
-            
-            
-            imprime_setor(i, digito, tam);
         }
     }
-    
+        
     return 0;
 }
